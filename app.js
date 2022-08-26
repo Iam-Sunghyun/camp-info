1bd6515b5460ccdbea226306e12906329050a80b
전성현 <sunghyun1148@gmail.com>
2022-08-26 23:46:13 +0900

    코드 추가

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const engine = require('ejs-mate'); // ejs 템플릿 엔진을 위한 Express.4.x 버전 레이아웃, 분할(particial), 블록(block) 함수 지원
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const appError = require('./utils/ExpressError');
+const asyncWrapper = require('./utils/asyncWrapper');
 
 // MongoDB 연결
 mongoose.connect('mongodb://localhost:27017/CampInfo')
@@ -26,51 +28,78 @@ app.engine('ejs', engine);
 // 요청 페이로드 파싱 설정
 app.use(express.urlencoded({ extended: true }));
 
-// 메서드 재정의 미들웨어 로드
+// method-override 모듈 쿼리 스트링 설정
 app.use(methodOverride('_method'));
+
+// 서버측 유효성 검사를 위한 모듈
+const joi = require('joi');
+const { campgroundSchema } = require('./schema');
+
+// joi 모듈을 이용한 요청 body 데이터 유효성 검증
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
 
 // 이하 기본적인 라우팅(CRUD)
 app.get('/', (req, res) => {
     res.render('home');
 });
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', asyncWrapper(async (req, res, next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-});
+}));
+
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, asyncWrapper(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.get('/campgrounds/:id', async (req, res,) => {
+app.get('/campgrounds/:id', asyncWrapper(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', asyncWrapper(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, asyncWrapper(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', asyncWrapper(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}));
+
+// 잘못된 경로 요청 처리용 라우터
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+// 에러 처리 미들웨어
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 
-
 app.listen(3000, () => {
-    console.log('Serving on port 3000');
+    console.log('Server listening on port 3000...');
 });
