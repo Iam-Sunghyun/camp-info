1d8634621c143c7a0f0ba1133dc8c29169bddc01
전성현 <sunghyun1148@gmail.com>
2022-09-12 23:19:47 +0900

    Mongoose 미들웨어 추가

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 // Mongoose 모델 파일
 const mongoose = require('mongoose');
+const Review = require('./review');
 
 const CampgroundSchema = new mongoose.Schema({
     title: String,
@@ -8,6 +9,15 @@ const CampgroundSchema = new mongoose.Schema({
     description: String,
     location: String,
     review: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
+});
+
+// findOneAndDelete은 findByIdAndDelete 쿼리 함수를 트리거한다.
+// 캠핑장 삭제 시, 달려있던 리뷰도 모두 삭제하는 미들웨어
+CampgroundSchema.post('findOneAndDelete', async (docs, next) => {
+  if (docs) {
+    await Review.deleteMany({ _id: { $in: docs.review } });
+  } 
+ 
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
