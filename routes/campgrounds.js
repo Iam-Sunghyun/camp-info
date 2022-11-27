56e8ac6480bf35d4ef44a010f9b6a4d251ff98ec
전성현 <sunghyun1148@gmail.com>
2022-11-27 23:34:26 +0900

    리뷰 라우터 분리

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Campground = require('../models/campground');
-const Review = require('../models/review');
 
 // 비동기(async) 라우트 핸들러 에러처리를 위한 모듈
 const catchAsyncError = require('../utils/catchAsyncError');
 
 // 서버 측 유효성 검사를 위한 joi 객체
-const { campgroundSchema, reviewSchema } = require('../schemas');
+const { campgroundSchema } = require('../schemas');
 
 /**
  * joi 모듈을 이용한 요청 페이로드 유효성 검증 미들웨어(새 캠핑장 추가)
@@ -32,17 +31,6 @@ const validateCampground = (req, res, next) => {
   }
 };
 
-// 리뷰 유효성 검증 미들웨어
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-      const msg = error.details.map(el => el.message).join(',');
-      throw new ExpressError(msg, 400);
-  } else {
-      next();
-  }
-}
-
 // 캠핑장 페이지
 router.get('/', catchAsyncError(async (req, res, next) => {
   const campgrounds = await Campground.find({});
@@ -61,34 +49,9 @@ router.post('/', validateCampground, catchAsyncError(async (req, res, next) => {
   res.redirect(`/${campground._id}`);
 }));
 
-// 리뷰 추가
-router.post('/:id/review', validateReview, catchAsyncError(async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  const review = new Review(req.body.review);
-  campground.review.push(review);
-  await Promise.all([review.save(), campground.save()]);  
-  res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-// 리뷰 삭제
-router.delete('/:campId/review/:reviewId', catchAsyncError(async (req, res, next) => {
-  const { reviewId, campId } = req.params;
-  const campground = await Campground.findById(campId);
-  // const review = campground['review'].indexOf(req.params.reviewId);
-  // campground['review'].splice(review, 1);
-
-  // $pull 연산자 -> 조건에 맞는 배열 필드의 요소를 모두 제거함.
-  await Promise.all([ Campground.updateOne(campground, { $pull: { review: reviewId } }),
-                      Review.findByIdAndDelete(reviewId),
-                      campground.save()
-                      ]);
-  res.redirect(`/campgrounds/${campground._id}`)
-}));
-
 // 캠핑장 삭제(mongoose 미들웨어로 달려있던 리뷰도 모두 삭제)
 router.delete('/:id', catchAsyncError(async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
+  await Campground.findByIdAndDelete(req.params.id);
   res.redirect('/campgrounds');
 }));
 
@@ -98,17 +61,16 @@ router.get('/:id', catchAsyncError(async (req, res, next) => {
   res.render('campgrounds/show', { campground });
 }));
 
-// 특정 캠핑장 세부화면에서 내용 수정
+// 특정 캠핑장 내용 수정 페이지
 router.get('/:id/edit', catchAsyncError(async (req, res, next) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground });
 }));
 
+// 특정 캠핑장 내용 수정
 router.put('/:id', validateCampground, catchAsyncError(async (req, res, next) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-  res.redirect(`/${campground._id}`);
+  const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`);
 }));
-
 
 module.exports = router;
