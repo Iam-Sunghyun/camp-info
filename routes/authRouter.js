b03f7baa0cca48cb8067299aa9e59ad824d288d8
전성현 <sunghyun1148@gmail.com>
2022-12-08 23:40:26 +0900

    passport-local-mongoose 회원가입 및 로그인 데모 추가

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -29,9 +29,18 @@ router.get('/signup', (req, res) => {
 
 // POST 회원가입
 router.post('/signup', async (req, res) => {
-  // passport-local-mongoose 모델 static 메서드
-  await User.register(new User({ username: req.body.username }, req.body.password));
-  res.redirect('/users/login');
+  try {
+    const { username, email, password } = req.body;
+    // email값이 중복되면 mongodb에서 아래와 같은 error 발생시킨다.. 흠
+    // MongoServerError: E11000 duplicate key error collection
+    await User.register(new User({ username, email }), password);   // passport-local-mongoose 모델 static 메서드
+    req.flash('success', '회원가입 성공!');
+    res.redirect('/campgrounds');
+  } catch (e) {
+    req.flash('error', e.message);
+    console.log(e)
+    res.redirect('/users/signup');
+  }
 });
 
 // GET 로그인 페이지
@@ -41,10 +50,17 @@ router.get('/login', (req, res) => {
 
 // POST 로그인 
 router.post('/login',
-  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-  catchAsyncError(async (req, res) => {
-  // await User.authenticate()(req.body.username, req.body.password); 이 방법도 가능한듯
-}));
+  // ↓ failureFlash: true일 경우 인증 에러시 플래시 메시지가 저장되는데 어떻게 내가만든 error 템플릿에 자동으로 출력이 되는가 확인해본 결과
+  // => req.flash('error')에 사용자가 입력한 failureFlash 메시지 혹은 기본 메시지가 저장되어 req.flash('error')로 참조 가능한 것.
+  // https://github.com/jaredhanson/passport/blob/master/lib/middleware/authenticate.js 124번줄 참조
+  // 그렇다면 req.flash는 connect-flash를 로드해야 사용 가능했는데, connect-flash와 passport의 관계는..?
+  // => express 3.x에서 req.flash()가 제거됨. 따라서 connect-flash 모듈을 사용해줘야 플래시 메시지 옵션을 사용할 수 있다.
+  passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: true }),
+  (req, res) => {
+    req.flash('success', `${req.body.username}님 환영합니다!`);
+    res.redirect('/campgrounds');
+  }
+);
 
 // 아이디 중복확인 라우터 하나 해야될듯
 
