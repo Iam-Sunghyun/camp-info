d6088884dd0dccc858ee2441784c79891d7f61c2
전성현 <sunghyun1148@gmail.com>
2022-12-17 23:36:34 +0900

    사용자 권한부여(Authorization) 추가

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ app.use(passport.session());
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error'); 
     next();
