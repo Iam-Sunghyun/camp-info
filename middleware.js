52890e65500594c4e02cbc8e1e7c0de9806d9b67
전성현 <sunghyun1148@gmail.com>
2022-12-21 23:57:35 +0900

    리뷰 삭제 권한부여

--- middleware.js
+++ middleware.js
@@ -55,3 +55,14 @@ module.exports.isAuthor = async (req, res, next) => {
   }
   next();
 };
+
+// 리뷰 작성자 확인 미들웨어
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that!');
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true }); // 중요※ 상위 라우터의 경로 매개변수(req.params) 값을 병합시킴. -> app.js의 라우터의 경로 매개변수를 사용할 수 있게 된다.
 const Campground = require('../models/campgroundModel');
 const Review = require('../models/reviewModel');
-const { isLoggedIn, validateReview } = require('../middleware');
+const { isLoggedIn, validateReview, isReviewAuthor } = require('../middleware');
 
 // 비동기(async) 라우트 핸들러 에러처리를 위한 모듈
 const catchAsyncError = require('../utils/catchAsyncError');
@@ -12,6 +12,7 @@ const catchAsyncError = require('../utils/catchAsyncError');
 router.post('/', isLoggedIn, validateReview, catchAsyncError(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
+  review.author = req.user._id;
   campground.review.push(review);
   await Promise.all([review.save(), campground.save()]);  
   req.flash('success', '리뷰가 성공적으로 등록되었습니다.');
@@ -19,7 +20,7 @@ router.post('/', isLoggedIn, validateReview, catchAsyncError(async (req, res) =>
 }));
 
 // 리뷰 삭제
-router.delete('/:reviewId', isLoggedIn, catchAsyncError(async (req, res, next) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsyncError(async (req, res, next) => {
   const { reviewId, id } = req.params;
   const campground = await Campground.findById(id);
 
