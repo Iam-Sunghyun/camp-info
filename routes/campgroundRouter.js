4c95e0a41154b37a39ae721e2b5faeb753ae12b4
전성현 <sunghyun1148@gmail.com>
2022-12-21 23:30:52 +0900

    미들웨어 분리

--- routes/campgroundRouter.js
+++ routes/campgroundRouter.js
@@ -1,38 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Campground = require('../models/campgroundModel');
-const ExpressError = require('../utils/ExpressError');
-const { isLoggedIn, isAuthor } = require('../middleware');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 
 // 비동기(async) 라우트 핸들러 에러처리를 위한 모듈
 const catchAsyncError = require('../utils/catchAsyncError');
 
-// 서버 측 유효성 검사를 위한 joi 객체
-const { campgroundSchema } = require('../schemas');
-
-/**
- * joi 모듈을 이용한 요청 페이로드 유효성 검증 미들웨어(새 캠핑장 추가)
- * - put, post요청 시 클라이언트 측에서 1차로 유효성 검증을 하지만 postman으로 페이로드에 필요한 일부 데이터를 누락시켜서 요청을 해도 그대로 동작을 하게된다.
- * 따라서 joi 모듈로 작성한 스키마로 2차로 서버 측에서 유효성 검사를 해준다.
- */
-const validateCampground = (req, res, next) => {
-  /** 
-   * 요청 body 데이터가 스키마에 맞는지 체크
-   * 정의한 스키마 유효성 검사에 통과하면 error에는 undefined가 할당되고
-   * 통과하지 못하면 에러 정보가 담긴 ValidationError 객체가 할당 됨
-   * joi 모듈 - https://joi.dev/api/?v=17.6.0#introduction
-   */
-  const { error } = campgroundSchema.validate(req.body);
-
-  if (error) {
-      // error 객체 details 프로퍼티의 모든 message 값을 가져와 커스텀 에러 클래스에 전달한다.
-      const msg = error.details.map(el => el.message).join(',');
-      throw new ExpressError(msg, 400);
-  } else {
-      next();
-  }
-};
 
 // 캠핑장 페이지
 router.get('/', catchAsyncError(async (req, res, next) => {
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -2,25 +2,11 @@ const express = require('express');
 const router = express.Router({ mergeParams: true }); // 중요※ 상위 라우터의 경로 매개변수(req.params) 값을 병합시킴. -> app.js의 라우터의 경로 매개변수를 사용할 수 있게 된다.
 const Campground = require('../models/campgroundModel');
 const Review = require('../models/reviewModel');
-const ExpressError = require('../utils/ExpressError');
-const { isLoggedIn } = require('../middleware');
+const { isLoggedIn, validateReview } = require('../middleware');
 
 // 비동기(async) 라우트 핸들러 에러처리를 위한 모듈
 const catchAsyncError = require('../utils/catchAsyncError');
 
-// 서버 측 유효성 검사를 위한 joi 객체
-const { reviewSchema } = require('../schemas');
-
-// 리뷰 유효성 검증 미들웨어
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-      const msg = error.details.map(el => el.message).join(',');
-      throw new ExpressError(msg, 400);
-  } else {
-      next();
-  }
-}
 
 // 리뷰 추가
 router.post('/', isLoggedIn, validateReview, catchAsyncError(async (req, res) => {
