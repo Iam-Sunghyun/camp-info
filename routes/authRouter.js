75bbed815c506cfb854a7b280cf3c68f371ace57
전성현 <sunghyun1148@gmail.com>
2022-12-26 23:52:07 +0900

    router.route()로 라우터 통합

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,34 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport')
+const passport = require('passport');
 const user = require('../controllers/authControl');
 
-// GET 회원가입 페이지
-router.get('/signup', user.renderSignup);
-
-// POST 회원가입
-router.post('/signup', user.signup);
+router.route('/signup')
+  .get(user.renderSignup) // GET 회원가입 페이지
+  .post(user.signup); // POST 회원가입
 
 // GET 로그인 페이지
-router.get('/login', user.login);
-
-// POST 로그인 
-router.post('/login',
-  // ↓ failureFlash: true일 경우 인증 에러시 플래시 메시지가 저장되는데 어떻게 내가만든 error 템플릿에 자동으로 출력이 되는가 확인해본 결과
-  // => 사용자가 입력한 메시지가 있다면 해당 메시지가, failureFlash 값이 boolean 이라면 기본 메시지가 키 값'error'(req.flash('error')) 저장된다.
-  // https://github.com/jaredhanson/passport  /blob/master/lib/middleware/authenticate.js 124번줄 참조
-  // 그렇다면 req.flash는 connect-flash를 로드해야 사용 가능했는데, connect-flash와 passport의 관계는..?
-  // => express 3.x에서 req.flash()가 제거됨. 따라서 3.x 이후 버전에선 connect-flash 모듈을 사용해줘야 플래시 메시지 옵션을 사용할 수 있다.
-  passport.authenticate('local', {
-    failureRedirect: '/users/login',
-    failureFlash: '아이디 혹은 비밀번호가 다릅니다.',
-    keepSessionInfo : true // ※ 이 옵션 기본 값이 false여서 req.session.returnTo가 리셋 됨. 그래서 원래 페이지로 redirect가 안됐던 것.
-  }), user.successLogin);
+router.route('/login')
+  .get(user.login) // GET 로그인 페이지
+  .post(
+    // POST 로그인
+    // ↓ failureFlash: true일 경우 인증 에러시 플래시 메시지가 저장되는데 어떻게 내가만든 error 템플릿에 자동으로 출력이 되는가 확인해본 결과
+    // => 사용자가 입력한 메시지가 있다면 해당 메시지가, failureFlash 값이 boolean 이라면 기본 메시지가 키 값'error'(req.flash('error')) 저장된다.
+    // https://github.com/jaredhanson/passport  /blob/master/lib/middleware/authenticate.js 124번줄 참조
+    // 그렇다면 req.flash는 connect-flash를 로드해야 사용 가능했는데, connect-flash와 passport의 관계는..?
+    // => express 3.x에서 req.flash()가 제거됨. 따라서 3.x 이후 버전에선 connect-flash 모듈을 사용해줘야 플래시 메시지 옵션을 사용할 수 있다.
+    passport.authenticate('local', {
+      failureRedirect: '/users/login',
+      failureFlash: '아이디 혹은 비밀번호가 다릅니다.',
+      keepSessionInfo: true, // ※ 이 옵션 기본 값이 false여서 req.session.returnTo가 리셋 됨. 그래서 원래 페이지로 redirect가 안됐던 것.
+    }),
+    user.successLogin
+  );
 
 // 로그아웃
 router.get('/logout', user.logout);
 
 // 아이디 중복확인 라우터 하나 해야될듯
-
 
 module.exports = router;
--- routes/campgroundRouter.js
+++ routes/campgroundRouter.js
@@ -1,31 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const campground = require('../controllers/campgroundsControl')
+const campground = require('../controllers/campgroundsControl');
 
 // 비동기(async) 라우트 핸들러 에러처리를 위한 모듈
 const catchAsyncError = require('../utils/catchAsyncError');
 
-
-// 캠핑장 페이지
-router.get('/', catchAsyncError(campground.index));
+router.route('/')
+  .get(catchAsyncError(campground.index)) // 캠핑장 페이지
+  .post(isLoggedIn, validateCampground, catchAsyncError(campground.createNewCampground)); // 새 캠핑장 추가
 
 // 새 캠핑장 추가 페이지
 router.get('/new', isLoggedIn, campground.renderCampgroundNew);
 
-// 새 캠핑장 추가
-router.post('/', isLoggedIn, validateCampground, catchAsyncError(campground.createNewCampground));
-
-// 캠핑장 삭제(mongoose 미들웨어로 달려있던 리뷰도 모두 삭제)
-router.delete('/:id', isLoggedIn, isAuthor, catchAsyncError(campground.deleteCampground));
-
-// 특정 캠핑장 세부화면
-router.get('/:id', catchAsyncError(campground.renderCampgroundDetail));
+router.route('/:id')
+  .get(catchAsyncError(campground.renderCampgroundDetail)) // 특정 캠핑장 세부화면
+  .put(isLoggedIn, isAuthor, validateCampground, catchAsyncError(campground.editCampground)) // 특정 캠핑장 내용 수정
+  .delete(isLoggedIn, isAuthor, catchAsyncError(campground.deleteCampground)); // 캠핑장 삭제(mongoose 미들웨어로 달려있던 리뷰도 모두 삭제)
 
 // 특정 캠핑장 내용 수정 페이지
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsyncError(campground.renderCampgroundEdit));
 
-// 특정 캠핑장 내용 수정
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsyncError(campground.editCampground));
 
 module.exports = router;
