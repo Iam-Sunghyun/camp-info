87eaf38ce506ba65c38bdfb018d8af8be94ee416
전성현 <sunghyun1148@gmail.com>
2022-12-31 23:34:23 +0900

    시드 데이터 이미지 필드 수정

--- seeds/index.js
+++ seeds/index.js
@@ -4,34 +4,46 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campgroundModel');
 
-mongoose.connect('mongodb://localhost:27017/CampInfo').then(() => {
-    console.log("MongoDB 연결 완료");
-}).catch(err => {
-    console.log("MongoDB 연결 실패");
+mongoose
+  .connect('mongodb://localhost:27017/CampInfo')
+  .then(() => {
+    console.log('MongoDB 연결 완료');
+  })
+  .catch((err) => {
+    console.log('MongoDB 연결 실패');
     console.log(err);
-});;
+  });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 // cities.js, seedHeplers.js 파일로 값 랜덤 조합
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 100000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            author: '6391dffd8a918d7d291e8007',
-            image: 'photo-1504851149312-7a075b496cc7.avif',
-            price,
-            description: 'Test text'
-        });
-        await camp.save();
-    }
+  await Campground.deleteMany({});
+  for (let i = 0; i < 10; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 100000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      author: '6391dffd8a918d7d291e8007',
+      price,
+      description: 'Test text',
+      image: [
+        {
+          url: 'https://res.cloudinary.com/dowpf7g5p/image/upload/v1672432725/CampInfo/vsk9ye7jop9erwenbxek.jpg',
+          filename: 'CampInfo/vsk9ye7jop9erwenbxek',
+        },
+        {
+          url: 'https://res.cloudinary.com/dowpf7g5p/image/upload/v1672432726/CampInfo/t6u2xmbyfmhhbmcwokte.jpg',
+          filename: 'CampInfo/t6u2xmbyfmhhbmcwokte',
+        },
+      ],
+    });
+    await camp.save();
+  }
 };
 
 seedDB().then(() => {
-    mongoose.connection.close();
-    console.log('시드 데이터 삽입 완료');
+  mongoose.connection.close();
+  console.log('시드 데이터 삽입 완료');
 });
