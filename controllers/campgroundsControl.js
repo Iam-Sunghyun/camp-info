ce431680932bba2050a5b8900348487d3ed5a41e
전성현 <sunghyun1148@gmail.com>
2023-01-06 23:24:09 +0900

    createNewCampground 라우트 핸들러에 Mapbox 지오코딩 추가

--- controllers/campgroundsControl.js
+++ controllers/campgroundsControl.js
@@ -1,4 +1,7 @@
 const Campground = require('../models/campgroundModel');
+const { cloudinary } = require('../cloudinary');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocoder = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -10,7 +13,12 @@ module.exports.renderCampgroundNew = (req, res) => {
 };
 
 module.exports.createNewCampground = async (req, res) => {
+  const geoData = await geocoder.forwardGeocode({
+    query: req.body.campground.location,
+    limit: 1
+  }).send();
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.image = req.files.map(f => ({
     url: f.path,
     filename: f.filename
@@ -56,6 +64,17 @@ module.exports.editCampground = async (req, res) => {
   }));
   campground.image.push(...addedFile);
   await campground.save();
+  // 삭제할 이미지가 있는 경우
+  if (req.body.deleteImage && req.body.deleteImage.length !== 0) {
+    for (const filename of req.body.deleteImage) {
+      // cloudinary에서 삭제
+      await cloudinary.uploader.destroy(filename);
+    }
+  // $pull 연산자로 지정된 조건과 일치하는 campground 도큐먼트의 image 필드 배열의 요소들을 제거
+  // 삭제 요소 조건은 image의 filename이 deleteImage[]의 요소 값과 일치하는 경우(deleteImagep[]의 요소는 사용자가 수정 페이지에서 체크박스로 체크한 이미지의 filename이다)
+  // 즉 사용자가 수정 페이지에서 체크박스로 체크한 이미지들을 campground의 image 필드 배열에서 삭제함
+    await campground.updateOne({ $pull: { image: { filename: { $in: req.body.deleteImage } } } });
+  }
   req.flash('success', '캠핑장 업데이트 완료!');
   res.redirect(`/campgrounds/${campground._id}`);
 };
