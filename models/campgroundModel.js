ea34a02d7aeaec9b121449263819908dd6dd10f0
전성현 <sunghyun1148@gmail.com>
2023-01-07 23:18:59 +0900

    CampgroundSchema toJSON 옵션 및 properties 가상 속성 추가

--- models/campgroundModel.js
+++ models/campgroundModel.js
@@ -38,6 +38,15 @@ const CampgroundSchema = new mongoose.Schema({
     review: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }] // ref: -> Populate 시 참조할 모델(컬렉션) 명(대문자 단수형)
 });
 
+// JSON.stringify()로 도큐먼트를 직렬화 했을 때 Mongoose는 virtuals를 포함시키지 않음.
+// 따라서 아래와 같이 직렬화(toJSON), 역직렬화(toObject)에 따라 virtulals: boolean 옵션을 전달해줘야 한다.
+// https://mongoosejs.com/docs/guide.html#virtuals
+CampgroundSchema.set('toJSON', { virtuals: true });
+
+CampgroundSchema.virtual('properties').get(function () {
+  return [this.title, this.description, this._id];
+});
+
 // findByIdAndDelete은 findOneAndDelete 함수를 트리거한다. -> findByIdAndDelete() 호출 시 아래 미들웨어 실행됨.
 // 캠핑장 삭제 시, 달려있던 리뷰도 모두 삭제하는 미들웨어
 CampgroundSchema.post('findOneAndDelete', async (docs, next) => {
