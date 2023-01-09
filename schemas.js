f4d29e9f1fc28e2ade87628bce1dcdafa746ca53
전성현 <sunghyun1148@gmail.com>
2023-01-09 23:26:31 +0900

    Joi 사용자 입력 HTML 금지 메서드 추가

--- schemas.js
+++ schemas.js
@@ -1,23 +1,45 @@
 // joi 모듈을 사용해 요청 데이터 스키마 유효성을 서버 측에서 검증한다.
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+    'string.escapeHTML': '{{#label}} must not include HTML!',
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value) return helpers.error('string.escapeHTML', { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 // 폼으로 전송되는 요청 body는 모두 campground[title], campground[price]..와 같이 전송하도록 했었다.
 // 따라서 다음과 같이 객체 안에 campground 객체가 들어있는 형태를 띈다.
 module.exports.campgroundSchema = Joi.object({
-    campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
-    }).required(),
-    deleteImage: Joi.array()
+  campground: Joi.object({
+    title: Joi.string().required().escapeHTML(),
+    price: Joi.number().required().min(0),
+    image: Joi.string(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
+  }).required(),
+  deleteImage: Joi.array(),
 });
 
-
 module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        body: Joi.string().required(),
-        rating: Joi.number().required().min(1).max(5),
-    }).required()
+  review: Joi.object({
+    body: Joi.string().required().escapeHTML(),
+    rating: Joi.number().required().min(1).max(5),
+  }).required(),
 });
